aa9933f19d005f893a63683e00a838e63857e2c2
Jerry Yu <zehua.yu@columbia.edu>
2025-03-31 09:33:21 +0800

    push, not done

---
+++ src/model.py
@@ -0,0 +1,67 @@
+import torch
+from torch import nn
+from monai.networks.nets import UNet
+import lightning.pytorch as pl
+
+
+def masked_mse_loss(y_hat, y, mask):
+    return torch.sum(torch.square(y[mask] - y_hat[mask]))
+
+
+class Model(pl.LightningModule):
+    def __init__(self, args):
+    
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = UNet(
+            spatial_dims=args.spatial_dims,
+            in_channels=args.in_channels,
+            out_channels=args.out_channels,
+            channels=args.channels,
+            strides=args.strides,
+            num_res_units=args.num_res_units,
+        )
+
+        self.train_loss = 0
+        self.valid_loss = 0
+        self.num_train_batch = 0
+        self.num_val_batch = 0
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y, mask = batch['image'], batch['label'], batch['mask']
+        y_hat = self(x)
+        loss = masked_mse_loss(y_hat, y, mask)
+        self.train_loss += loss
+        self.num_train_batch += 1
+        torch.cuda.empty_cache()
+        return loss
+
+    def on_train_epoch_end(self):
+        loss_per_epoch = self.train_loss/self.num_train_batch
+        print(f"Epoch {self.current_epoch} - Average Train Loss: {loss_per_epoch:.4f}")
+        self.log('train_loss', loss_per_epoch, prog_bar=False)
+        self.train_loss = 0
+        self.num_train_batch = 0
+    
+    def validation_step(self, batch, batch_idx):
+        with torch.no_grad(): # This ensures that gradients are not stored in memory
+            x, y, mask = batch['image'], batch['label'], batch['mask']
+            y_hat = self(x)
+            loss = masked_mse_loss(y_hat, y, mask)
+            self.valid_loss += loss
+            self.num_val_batch += 1
+        torch.cuda.empty_cache()
+        return {'val_loss': loss}
+
+    def on_validation_epoch_end(self):
+        loss_per_epoch = self.val_loss/self.num_val_batch
+        print(f"Epoch {self.current_epoch} - Average Val Loss: {loss_per_epoch:.4f}")
+        self.log('val_loss', loss_per_epoch, prog_bar=False, sync_dist=False)
+        self.valid_loss = 0
+        self.num_val_batch = 0
+    
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=self.hparams.args.lr)
---
+++ src/train.py
@@ -0,0 +1,138 @@
+from utils import *
+from model import *
+import numpy as np
+import torch
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+import os
+
+from monai.data import DataLoader, Dataset, CacheDataset, decollate_batch
+from monai.transforms import (
+    Compose, 
+    EnsureChannelFirstd, 
+    Orientationd,  
+    AsDiscrete,  
+    RandFlipd, 
+    RandRotate90d, 
+    NormalizeIntensityd,
+    RandCropByLabelClassesd,
+)
+
+
+def get_pcb_images():
+    from PIL import Image
+    PATH_DATA = "/root/autodl-tmp/kaggle/anomaly/pcb2/mixed"
+    filenames = os.listdir(PATH_DATA)
+    filenames = [f for f in filenames if f.endswith(".JPG")]
+    filenames = [f for f in filenames if len(f) == 8]  # only normal samples
+    all_images = []
+    for filename in filenames:
+        image = Image.open(os.path.join(PATH_DATA, filename))
+        image = np.array(image)  # (H,W,C)
+        image = np.moveaxis(image, 2, 0)  # (C,H,W)
+        all_images.append(image)
+    return all_images
+    
+
+class GetMask(object):
+    # transform. given image, generate random mask.
+    def __init__(self, k, nfolds):
+        self.k = k
+        self.nfolds = nfolds
+
+    def __call__(self, sample):
+        sample = sample['image']
+        c, h, w = sample.shape
+        assert h % self.k == 0
+        assert w % self.k == 0
+        hk = h / self.k
+        wk = w / self.k
+        mask = np.full((hk * wk,), False, dtype=np.bool)
+        mask[:int(hk * wk / self.nfolds)] = True
+        mask = np.permutation(mask)
+        mask = mask.reshape(hk, wk)
+        mask = np.repeat(mask, self.k, axis=0)
+        mask = np.repeat(mask, self.k, axis=1)
+        return {'image': sample, 'mask': mask}
+
+    
+def get_pcb_loaders(train_images, valid_images):
+    TRAIN_BATCH_SIZE = 16
+    VALID_BATCH_SIZE = 64
+
+    transforms = Compose([
+        NormalizeIntensityd(keys="image"),
+        # Orientationd(keys=["image", "label"], axcodes="RAS"),
+        GetMask(k=4, nfolds=5)
+    ])
+    train_ds = Dataset(data=train_images, transform=transforms)
+    # TODO: validation should be non random. how??
+    valid_ds = Dataset(data=valid_images, transform=transforms)
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=TRAIN_BATCH_SIZE,
+        shuffle=True,
+        num_workers=32,
+        pin_memory=True
+    )
+    valid_loader = DataLoader(
+        valid_ds,
+        batch_size=VALID_BATCH_SIZE,
+        shuffle=False,
+        num_workers=32,
+        pin_memory=torch.cuda.is_available()
+    )
+    return train_loader, valid_loader
+
+
+def train_pcb():
+    ARGS = DotDict(
+        spatial_dims=2,
+        in_channels=3,
+        out_channels=3,
+        channels=(48, 64, 80, 80),
+        strides=(2, 2),
+        num_res_units=1,
+        lr=1e-3,
+    )
+    NUM_EPOCH = 1000
+
+    all_images = get_pcb_images()
+    n = len(all_images)
+    train_idx = np.arange(n)[:int(n * 0.8)]
+    valid_idx = np.arange(n)[int(n * 0.8):]
+    train_images = [all_images[i] for i in train_idx]
+    valid_images = [all_images[i] for i in valid_idx]
+    train_loader, valid_loader = get_pcb_loaders(train_images, valid_images)
+    model = Model(ARGS)
+    torch.set_float32_matmul_precision('medium')
+    checkpoint = pl.callbacks.ModelCheckpoint(
+        save_last=True,
+        save_top_k=1, 
+        monitor='val_loss', 
+        mode='min'
+    )
+    early_stopping = EarlyStopping(
+        monitor="val_loss", 
+        mode="min", 
+        patience=20
+    )
+    trainer = pl.Trainer(
+        max_epochs=NUM_EPOCH,
+        accelerator="gpu",
+        devices=[0],
+        num_nodes=1,
+        log_every_n_steps=10,
+        enable_progress_bar=False,
+        callbacks=[
+            checkpoint, 
+            early_stopping, 
+        ],
+        default_root_dir="/root/autodl-tmp/kaggle/anomaly/jerry_anomaly"
+    )
+    trainer.fit(model, train_loader, valid_loader)
+
+
+if __name__ == "__main__":
+    train_pcb()
